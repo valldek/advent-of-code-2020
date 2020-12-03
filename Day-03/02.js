2b929edd13fbdac7456de88e4d75718f94f8e078
valldek <valldek@gmail.com>
2020-12-03 17:48:16 +0100

    Day 03 PART II solution

--- Day-03/02.js
+++ Day-03/02.js
@@ -4,15 +4,27 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
     dataArr = data.split('\n');
-    let down = 1;
-    let right = 3;
 
-    const reduced = dataArr.reduce((acc, cur, idx) => {
-        right = (idx * 3) % 31
-        return acc = acc + cur[right];
-    }, '').replace(/\./g, '').length;
+    const reduced11 = throughtSloups(dataArr, 1);
+    const reduced31 = throughtSloups(dataArr, 3);
+    const reduced51 = throughtSloups(dataArr, 5);
+    const reduced71 = throughtSloups(dataArr, 7);
+    const reduced12 = throughtSloups(dataArr.filter((val, idx) => !(idx % 2)), 1);
     
-    console.log(reduced);
+    output = reduced11 * reduced31 * reduced51 * reduced71 * reduced12;
+    
+    console.log(output);
+}
+
+const throughtSloups = (arr, rightBy) => {
+    
+        return arr.reduce((acc, cur, idx) => {
+            const right = (idx * rightBy) % 31;
+            return acc = acc + cur[right];
+        }, '')
+        .replace(/\./g, '')
+        .length;
+    
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
