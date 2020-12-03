dd47179292720ab4f4e7dfd07737379410814ad3
valldek <valldek@gmail.com>
2020-12-03 09:42:08 +0100

    First part of Day 01

---
+++ Day-01/01.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const dataRead = (err, data) => {
+    if (err) throw error;
+    let response;
+
+    const dataArrNum = data.split('\n').map(value => +value);
+
+    const output = dataArrNum.filter((value, idx, arr) => {
+        return arr.filter(val => 2020 - value === val).length 
+    });    
+
+
+    if (output.length === 2) {
+        response = output.reduce((acc, cur) => {
+            return acc = acc * cur 
+        }, 1);
+    }
+    console.log(
+        ' numbers: ', output,'\n', 
+        'response: ', response,     
+    );
+}
+
+fs.readFile(path.join(__dirname, 'input'), 'utf-8', dataRead);
