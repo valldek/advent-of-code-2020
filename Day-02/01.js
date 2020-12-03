cac8505eead5bf93ea4ad953198b5c3f031f8b77
valldek <valldek@gmail.com>
2020-12-03 14:15:45 +0100

    Day 02 Part | solution

--- Day-02/01.js
+++ Day-02/01.js
@@ -4,7 +4,22 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
 
-    console.log(data);
+    const dataArrStr = data.trim().split('\n')
+        .map(val => val.split(' '))
+        .filter(item => {
+            const range = item[0].split('-').map(val => +val);
+            const character = item[1][0];
+            const testString = item[2];
+
+            const regex = new RegExp("[^" + character +"]", "g");
+            const testStringWithout = testString.replace(regex, '');
+            
+            return testStringWithout.length >= range[0] && testStringWithout.length <= range[1];
+        })
+        .length;
+
+
+    console.log(dataArrStr);
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
