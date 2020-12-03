ebc201fd04641b74f32f92e57dacb6bd0f0597ea
valldek <valldek@gmail.com>
2020-12-03 17:09:03 +0100

    Day 03 PART I - solution

--- Day-03/01.js
+++ Day-03/01.js
@@ -3,7 +3,16 @@ const path = require('path');
 
 const callback = (err, data) => {
     if (err) throw err;
-    console.log(data);
+    dataArr = data.split('\n');
+    let down = 1;
+    let right = 3;
+
+    const reduced = dataArr.reduce((acc, cur, idx) => {
+        right = (idx * 3) % 31
+        return acc = acc + cur[right];
+    }, '').replace(/\./g, '').length;
+    
+    console.log(reduced);
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
