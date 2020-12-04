07900a2516645419f323eddfb11416a2cb03f4da
valldek <valldek@gmail.com>
2020-12-04 06:55:41 +0100

    Day 04 PART I file read

---
+++ Day-04/01.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const callback = (err, data) => {
+    console.log(data);
+}
+
+fs.readFile(path.join(__dirname, input), 'utf-8', callback);
