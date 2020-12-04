aefa0c136dcfc9609509256ac99abbd33ad59d03
valldek <valldek@gmail.com>
2020-12-04 08:52:50 +0100

    Day 04 PART II solution

--- Day-04/02.js
+++ Day-04/02.js
@@ -14,16 +14,64 @@ const callback = (err, data) => {
     });
 
     const output = dataArrOfObj.filter(value => {
-        return value.hasOwnProperty('byr') &&
-        value.hasOwnProperty('iyr') &&
-        value.hasOwnProperty('eyr') &&
-        value.hasOwnProperty('hgt') &&
-        value.hasOwnProperty('hcl') &&
-        value.hasOwnProperty('ecl') &&
-        value.hasOwnProperty('pid')
+        return value.hasOwnProperty('byr') && check4DigitNumbers(value.byr, 1920, 2002) &&
+        value.hasOwnProperty('iyr') && check4DigitNumbers(value.iyr, 2010, 2020) &&
+        value.hasOwnProperty('eyr') && check4DigitNumbers(value.eyr, 2020, 2030) &&
+        value.hasOwnProperty('hgt') && checkHeight(value.hgt) &&
+        value.hasOwnProperty('hcl') && checkHairColor(value.hcl) &&
+        value.hasOwnProperty('ecl') && checkEyeColor(value.ecl) &&
+        value.hasOwnProperty('pid') && checkPassportID(value.pid)
     }).length;
 
     console.log(output);
+}
+
+const check4DigitNumbers = (input, min, max) => {
+    if (!Number.isInteger(+input)) {
+        return false;
+    }
+
+    if (input.length !== 4) {
+        return false;
+    }
+
+    if (input >= min && input <= max) {
+        return true;
+    }
+    
+    return false;
+}
+
+const checkHeight = (input) => {
+    if (input.match(/\b\d+(in)\b/)) {
+        const inches = input.replace('in', '');
+        return inches >= 59 && inches <= 76;
+    } else if (input.match(/\b\d+(cm)\b/)) {
+        const inches = input.replace('cm', '');
+        return inches >= 150 && inches <= 193;
+    };
+
+    return false;
+}
+
+const checkHairColor = (input) => {
+    return input.match(/\#[0-9a-f]{6}/);
+}
+
+const checkEyeColor = (input) => {
+    return input.match(/\b(amb|blu|brn|gry|grn|hzl|oth)\b/);
+}
+
+const checkPassportID = (input) => {
+    if (!Number.isInteger(+input)) {
+        return false;
+    }
+
+    if (input.length !== 9) {
+        return false;
+    }
+
+    return true;
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
