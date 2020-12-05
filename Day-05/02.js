5d83aa307f143d046ffa1e938ee9a30ccc49a47d
valldek <valldek@gmail.com>
2020-12-05 13:27:05 +0100

    Day 05 PART II solution

--- Day-05/02.js
+++ Day-05/02.js
@@ -5,6 +5,7 @@ const callback = (err, data) => {
     if (err) throw err;
 
     const dataArr = data.split('\n');
+    const planeSeats = Array.from({length: 127}, e => Array(8).fill(0));
 
     const output = dataArr.map(seat => {
         const row = [0, 127];
@@ -21,11 +22,20 @@ const callback = (err, data) => {
                 col[0] = Math.ceil((col[0] + col[1]) / 2);
             }
         }
-        
+        planeSeats[row[0]][col[0]] = 1;
         return row[0] * 8 + col[0];
     })
 
-    console.log(Math.max(...output));
-}
+    // console.log(planeSeats);
+    const myRow = planeSeats.map(
+        col => col.reduce((acc, cur) => {
+            return acc = acc + cur;
+        }, 0)
+    ).findIndex(val => val === 7);
+
+    const myCol = planeSeats[myRow].findIndex(col => col === 0);
+
+    console.log(myRow, myCol, myRow * 8 + myCol);
+}   
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
