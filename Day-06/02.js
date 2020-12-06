b25a50538f04645c200500f1fb21163f8649aff8
valldek <valldek@gmail.com>
2020-12-06 15:00:57 +0100

    Day 06 PART II solution

--- Day-06/02.js
+++ Day-06/02.js
@@ -4,12 +4,24 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
 
-    const dataSplit = data.split('\n\n').map(val => (new Set(val.split('\n').join(''))).size);
+    const dataSplit = data.split('\n\n').map(val => {
+        const answersOfPersons = val.split('\n');
+        const groupSize = answersOfPersons.length;
+        const answersOfAll = answersOfPersons.join('');
+        const answersCount = answersOfAll.split('').reduce((acc, cur) => {
+            acc[cur] ? acc[cur]++ : acc[cur] = 1;
+            return acc;
+        }, {})
+        
+        const allYes = Object.values(answersCount).filter(val => val === groupSize);
+
+        return allYes.length;
+    });
 
     const output = dataSplit.reduce((acc, cur) => 
         acc = acc + cur
-    , 0);
-
+        , 0);
+        
     console.log(output);
 }
 
