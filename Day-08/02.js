b2e0362c8f497119bf221192cdf1537d861f0fd0
valldek <valldek@gmail.com>
2020-12-08 21:42:13 +0100

    Day 08 Part II clumsy Solution

---
+++ Day-08/02.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const callback = (err, data) => {
+    if (err) throw err;
+    
+    const dataArr = data.split('\n');
+    const dataArrSize = dataArr.length;
+    const dataMtrx = [];
+
+    for (let i = 0; i < dataArr.length; i++) {
+        const tmpDataArr = [...dataArr];
+        const [instruction, operation] = tmpDataArr[i].split(' ');
+
+        if (instruction !== 'acc') {
+            tmpDataArr[i] = instruction === 'nop' ? `jmp ${operation}` : `nop ${operation}`;
+        } else {
+            continue
+        }
+
+        console.log((checkIfCorrect(tmpDataArr, dataArrSize)));
+
+    }
+
+    // console.log(dataMtrx[1], dataMtrx[5]);
+
+}
+
+function checkIfCorrect(arr, termination) {
+
+    let accumulator = 0;
+    let idx = 0;
+    let processed = [];
+
+    while (!processed[idx]) {
+        if (idx === termination) {
+            break;
+        }
+        processed[idx] = true;
+        const [instruction, operation] = arr[idx].split(' ');
+        if (instruction === 'acc') {
+            accumulator += +operation;
+            idx = idx + 1;
+        } else if (instruction === 'jmp') {
+            idx += +operation;
+        } else if (instruction === 'nop') {
+            idx = idx + 1;
+        }
+    }
+
+    return [idx === termination, accumulator];
+}
+
+
+fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
