363e7f35060c9e44451573e63bdd932e639e77e9
valldek <valldek@gmail.com>
2020-12-09 10:57:30 +0100

    Day 09 PART II correct answer

--- Day-09/02.js
+++ Day-09/02.js
@@ -3,14 +3,36 @@ const path = require('path');
 
 const callback = (err, data) => {
     dataArrNum = data.split('\n').map(val => parseInt(val, 10));
+    let invalidNumber = 0;
+    let range = [];
 
     for (let i = 0; i < dataArrNum.length - 25; i++) {
         const valid = sumOfPreamble(dataArrNum.slice(i, i + 25), dataArrNum[i + 25]);
         if (!valid) {
-            console.log(dataArrNum[i + 25]);
+            invalidNumber = dataArrNum[i + 25];
             break;
         }
     }
+
+    const invalidNumberIndex = dataArrNum.indexOf(invalidNumber);
+    const dataArrToInvalid = dataArrNum.slice(0, invalidNumberIndex);
+
+    for (let i = 0; i < dataArrToInvalid.length; i ++ ) {
+        const check = sumAll(dataArrToInvalid.slice(i), invalidNumber);
+
+        if (check[0]) {
+            range = [i, check[1]];
+            break;
+        }
+    }
+
+    const responseRange = dataArrToInvalid.slice(range[0], range[0] + range[1] + 1);
+    const rangeSum = responseRange.reduce((acc, cur) => {
+        acc = acc + cur;
+        return acc;
+    }, 0);
+
+    console.log(Math.min(...responseRange) + Math.max(...responseRange));
 }
 
 const sumOfPreamble = (arr, num) => {
@@ -21,6 +43,24 @@ const sumOfPreamble = (arr, num) => {
     });
 
     return isSum.length ? num : false;
+}
+
+const sumAll = (arr, num) => {
+    let idx = 0;
+    let sum = 0;
+    let response = [false, idx];
+
+    while(sum < num) {
+        sum = sum + arr[idx];
+        
+        if ( sum === num) {
+            response = [true, idx];
+            break;
+        }        
+        idx++;        
+    }
+
+    return response;
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
