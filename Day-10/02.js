b3093d0865ce19daaa66f1900a670d5c65e3dc12
valldek <valldek@gmail.com>
2020-12-10 23:50:06 +0100

    Solution for smaler input - test and short

--- Day-10/02.js
+++ Day-10/02.js
@@ -10,16 +10,35 @@ const callback = (err, data) => {
 
     dataArrSorted = data.split('\n')
         .map(val => parseInt(val, 10))
-        .sort((a, b) => a - b)
-        .reduce((acc, cur) => {
-            const diff = cur - acc;
-            output[diff] ? output[diff] += 1 : output[diff] = 1;
+        .sort((a, b) => a - b);
     
-            return cur;
-        }, 0);
+    let levelToChek = [0];
+    let out = [];
 
-    console.log(output);
-    console.log(output[1] * output[3]);
+    for (let j = 0; j < dataArrSorted.length; j++) {
+        levelToChek = findNextLevel(dataArrSorted, levelToChek);
+        console.log(j, levelToChek);
+    }
+
+    console.log(levelToChek.length);
+}
+
+const findNextLevel = (arr, current) => {
+    const last = arr[arr.length - 1];
+    let nextLevel = [];
+    for (let i = 0; i < current.length; i++) {
+        const currentIndex = arr.indexOf(current[i]);
+        if (currentIndex === -1) continue;
+        if (current[i] === last) nextLevel.push(current[i]);
+        const next = arr
+            .slice(currentIndex + 1, currentIndex + 4)
+            .filter(val => {
+                return val <= current[i] + 3;
+            });
+        nextLevel.push(...next);
+    }
+
+    return nextLevel;
 }
 
 const task = fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
