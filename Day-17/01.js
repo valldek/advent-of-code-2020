03c58653b26b100000c7abc2172ffad83e5a1141
valldek <valldek@gmail.com>
2020-12-26 01:15:26 +0100

    Day 17 PART I Solution

--- Day-17/01.js
+++ Day-17/01.js
@@ -4,90 +4,97 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
 
-    const initialState = pocketInitialization(data);
-    const next = nextCycle(initialState);
+    const cycleCount = 6;
 
-    console.log(next);
-}
+    const initialState = pocketInitialization(data);
+    let state = initialState;
+
+    for (let i = 0; i < cycleCount; i ++) {
+        state = nextCycle(state);
+    }
+
+    let sum = 0;
+    let cubes = state.values();
+    for (const cube of cubes) {
+        if(cube === '#') sum++;
+    }
+
+    console.log(sum);
+};
 
 const pocketInitialization = (input) => {
     const initialState = new Map();
-    const rows = input.split('\n').map(val => val.split(''));
+    const rows = input.split('\n').map((val) => val.split(''));
     rows.forEach((row, y) => {
         row.forEach((col, x) => {
-            const active = col === '#';
-            const key = [x, y, 0].join('');
+            const active = col === '#' ? '#' : '.';
+            const key = [0, y, x].join(',');
             initialState.set(key, active);
-        })
-    });  
+        });
+    });
     return initialState;
-}
+};
 
 const nextCycle = (state) => {
     const newState = new Map();
     const minMax = expandRegion(state);
-    console.log(minMax)
-    for (let x = minMax[0]; x <= minMax[1]; x++) {
+
+    for (let z = minMax[0]; z <= minMax[1]; z++) {
         for (let y = minMax[2]; y <= minMax[3]; y++) {
-            for (let z = minMax[4]; z <= minMax[5]; z++) {
-                const neighbors = getNeighbors(x, y, z, state);
-                const activeNeibors = neighbors.filter(x => x).length;
-                const key = [x, y, z].join(`,`);
-                const isActive = state.has(key) ? state.get(key) : false;
-                if (isActive && activeNeibors !== 2 && activeNeibours !== 3) {
-                    newState.set(key, false);
+            for (let x = minMax[4]; x <= minMax[5]; x++) {
+                const neighbors = getNeighbors(z, y, x, state);
+                const activeNeibors = neighbors.filter((x) => x === '#').length;
+                const key = [z, y, x].join(`,`);
+                const activeKey = state.has(key) ? state.get(key) : '.';
+                const isActive = activeKey === '#';
+                if (isActive && activeNeibors !== 2 && activeNeibors !== 3) {
+                    newState.set(key, '.');
                 } else if (!isActive && activeNeibors === 3) {
-                    newState.set(key, true);
+                    newState.set(key, '#');
                 } else {
-                    newState.set(key, isActive);
+                    newState.set(key, activeKey);
                 }
             }
         }
     }
     return newState;
-}
+};
 
 const expandRegion = (state) => {
-    // console.log(state.keys());
-
-    // const keys = state.keys();
     let minMax = Array.from({length: 6}, () => 0);
 
     state.forEach((val, key) => {
-        // console.log(typeof key);
-        const [x, y, z] = key.split('').map(k => parseInt(k, 10));
-        if (x < minMax[0]) minMax[0] = x;
-        if (x > minMax[1]) minMax[1] = x;
+        const [z, y, x] = key.split(',').map(k => parseInt(k, 10));
+        if (z < minMax[0]) minMax[0] = z;
+        if (z > minMax[1]) minMax[1] = z;
         if (y < minMax[2]) minMax[2] = y;
         if (y > minMax[3]) minMax[3] = y;
-        if (z < minMax[4]) minMax[4] = z;
-        if (z > minMax[5]) minMax[5] = z; 
+        if (x < minMax[4]) minMax[4] = x;
+        if (x > minMax[5]) minMax[5] = x;
     });
 
-
     return minMax.map((val, idx) => {
-        return idx % 2 ? val + 1 : val - 1; 
+        return idx % 2 ? val + 1 : val - 1;
     });
 }
 
-const getNeighbors = (x, y, z, state) => {
-    const neighbors = [];
-    for (let i = x - 1; i <= x + 1; i++) {
+const getNeighbors = (z, y, x, state) => {
+    const neighbors = []
+    for (let i = z - 1; i <= z + 1; i++) {
         for (let j = y - 1; j <= y + 1; j++) {
-            for (let k = z - 1; k <= z + 1; k++) {
-                if (i != x || j != y || k != z) {
-                    const key = [i, j, k].join(`,`)
+            for (let k = x - 1; k <= x + 1; k++) {
+                if (i !== z || j !== y || k !== x) {
+                    const key = [i, j, k].join(`,`);
                     if (state.has(key)) {
                         neighbors.push(state.get(key));
                     } else {
-                        neighbors.push(false);
+                        neighbors.push('.');
                     }
                 }
             }
         }
-
     }
     return neighbors;
-}
+};
 
-fs.readFile(path.join(__dirname, 'testInput'), 'utf-8', callback);
+fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
