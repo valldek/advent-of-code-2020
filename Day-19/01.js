858488e38f1596e9708aadbdb4b1d6e1226fb619
valldek <valldek@gmail.com>
2020-12-27 23:58:22 +0100

    Day 19 PART I testInput

--- Day-19/01.js
+++ Day-19/01.js
@@ -4,11 +4,17 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
 
-    const inputMap = getMapFromInput(data);
-    output = goThroughtTheMap(inputMap, '0');
+    const inputData = data.split('\n\n');
 
+    const inputMap = getMapFromInput(inputData[0]);
+    const inputMessages = inputData[1].split('\n');
+    const zeroMap = reduceMap(inputMap);
+    const regExp = zeroMap.get('0').replace(/\s/g, '');
+
+    const output = countMessagesMatchingRule(regExp, inputMessages);
+
+    console.log(regExp);
     console.log(output);
-
 };
 
 const getMapFromInput = (input) => {
@@ -18,7 +24,7 @@ const getMapFromInput = (input) => {
     inputArr.forEach((row) => {
         const splitedRow = row.split(':');
 
-        if (splitedRow[1].indexOf("\"") !== -1) {
+        if (splitedRow[1].indexOf('"') !== -1) {
             splitedRow[1] = splitedRow[1].replace(/\"/g, '');
         }
 
@@ -28,33 +34,42 @@ const getMapFromInput = (input) => {
     return inputMap;
 };
 
-const goThroughtTheMap = (map, key) => {
-    let result = [];
-    let resultIndex = 0;
-
-    if (map.has(key)) {
-        result[resultIndex] += goThroughtTheMap(map, map.get(key));
-    } else {
-        if (key === 'a' || key === 'b') {
-            result[resultIndex] += key;
-        } else {
-            const orInKey = key.indexOf('|') === -1 ? false : true;
-            if (orInKey) {
-                const keysArr = key.split('|');
-                keysArr.forEach((keys, idx) => {
-                    resultIndex = idx;
-                    result[resultIndex] += goThroughtTheMap(map, keys);
-                })
-            } else {
-                const keys = key.trim().split(' ');
-                keys.forEach(k => {
-                    result[resultIndex] += goThroughtTheMap(map, k);
-                })
+const reduceMap = (map) => {
+    while (map.size > 1) {
+        const keys = findKeysForValuesWithoutNumbers(map);
+        console.log(keys);
+        keys.forEach((key) => {
+            let value = map.get(key);
+            if (value.length > 1) {
+                value = `(${value})`;
             }
-        }
+
+            map.delete(key);
+            map.forEach((v, k) => {
+                v = v.replace(new RegExp(key, 'g'), value);
+                map.set(k, v);
+            });
+        });
     }
+    return map;
+};
 
-    return result;
-}
+const findKeysForValuesWithoutNumbers = (map) => {
+    const keys = [];
+    map.forEach((value, key) => {
+        if (!/[\d+]/.test(value)) {
+            keys.push(key);
+        }
+    });
 
-fs.readFile(path.join(__dirname, 'shortInput'), 'utf-8', callback);
+    return keys;
+};
+
+const countMessagesMatchingRule = (rule, messages) => {
+    const ruleRegExp = new RegExp(`^${rule}$`);
+    return messages.filter((value) => {
+        return ruleRegExp.test(value)
+    }).length;
+};
+
+fs.readFile(path.join(__dirname, 'testInput'), 'utf-8', callback);
