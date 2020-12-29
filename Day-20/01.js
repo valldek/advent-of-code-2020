09332ab3f95ff8db5bb28a9ee717158bde610a97
valldek <valldek@gmail.com>
2020-12-29 15:06:54 +0100

    Day 20 PART I Solution

--- Day-20/01.js
+++ Day-20/01.js
@@ -5,9 +5,29 @@ const callback = (err, data) => {
     if (err) throw err;
 
     const tilesArr = data.split('\n\n');
-    const tilesBorder = tilesArr.map((tile) => parseTile(tile));
+    const tiles = tilesArr.map((tile) => parseTile(tile));
+    const tilesCount = tiles.length;
 
-    console.log(tilesBorder);
+    tiles.forEach((tile, idx, arr) => {
+        tile.mutualEdge = [];
+        tile.borderPossibility.forEach((edge) => {
+            arr.forEach((otherTile, index) => {
+                if (index !== idx) {
+                    if(otherTile.borderPossibility.includes(edge)) {
+                        tile.mutualEdge.push(edge);
+                    }
+                }
+            })
+        })
+        tile.mutualEdgeCount = tile.mutualEdge.length / 2;
+    });
+
+    const cornerTiles = tiles.filter((tile) => tile.mutualEdgeCount === 2);
+    const cornerTilesNames = cornerTiles.map((tile) => parseInt(tile.name), 10);
+    const output = cornerTilesNames.reduce((acc, cur) => acc *= cur, 1);
+
+    console.log(cornerTilesNames);
+    console.log(output);
 };
 
 const parseTile = (str) => {
@@ -29,10 +49,19 @@ const parseTile = (str) => {
         tileBorder.left += line[0];
     });
 
+    const tileBorderTransform = {
+        top: tileBorder.top.split('').reverse().join(''),
+        right: tileBorder.right.split('').reverse().join(''),
+        bottom: tileBorder.bottom.split('').reverse().join(''),
+        left: tileBorder.left.split('').reverse().join(''),
+    }
+
     return {
         name: tileName,
-        border: tileBorder
+        border: tileBorder,
+        borderTransform: tileBorderTransform,
+        borderPossibility: [...Object.values(tileBorder), ...Object.values(tileBorderTransform)]
     };
 };
 
-fs.readFile(path.join(__dirname, 'testInput'), 'utf-8', callback);
+fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
