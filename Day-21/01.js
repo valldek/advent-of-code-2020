310f09b4e54f357d07e90a19d7fe3ee918fc4959
valldek <valldek@gmail.com>
2020-12-30 17:30:56 +0100

    Day 21 PART I Solution

--- Day-21/01.js
+++ Day-21/01.js
@@ -4,7 +4,76 @@ const path = require('path');
 const callback = (err, data) => {
     if (err) throw err;
 
-    console.log(data);
+    const parsedInput = parseInput(data);
+    const possible = getAllergentPossibleIngredients(parsedInput);
+
+    console.log(parsedInput);
+
+    // console.log(getAllIngredients(parsedInput));
+
+    const withAllergent = getAllIngredientsWithAllergent(possible);
+
+    console.log(withAllergent);
+
+    const output = getAllIngredients(parsedInput)
+        .ingredients
+        .filter((ingredient) => {
+            return !withAllergent.has(ingredient)
+        });
+
+    console.log(output.length);
+
 }
 
-fs.readFile(path.join(__dirname, 'testinput'), 'utf-8', callback);
+const parseInput = (input) => {
+    const inputLine = input.split('\n');
+
+    return inputLine.map((line) => {
+        const [ingredientsStr, allergensStr] = line.replace('\)', '').split(' (contains ');
+        return {
+            ingredients: ingredientsStr.split(' '),
+            allergens: allergensStr.split(', ')
+        }
+    });
+}
+
+const getAllergentPossibleIngredients = (input) => {
+    const possible = {};
+
+    input.forEach((ingAll) => {
+        ingAll.allergens.forEach((allergen) => {
+            if (possible[allergen]) {
+                possible[allergen] = ingAll.ingredients.filter((ing) => {
+                        return possible[allergen].includes(ing);
+                    })
+            } else {
+                possible[allergen] = ingAll.ingredients;
+            }
+        })
+    });
+
+    return possible;
+}
+
+const getAllIngredientsWithAllergent = (possible) => {
+    const ingredients = Object.values(possible).flat();
+
+    return new Set(ingredients);
+}
+
+const getAllIngredients = (input) => {
+    const ingredients = [];
+
+    input.forEach((ingAll) => {
+        ingredients.push(...ingAll.ingredients)
+    });
+
+    return {
+        uniqueIngredients: new Set(ingredients),
+        ingredients
+    }
+}
+
+
+
+fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
