90c5e0a1ba61f797d8f480589599b9f43daf8072
valldek <valldek@gmail.com>
2020-12-30 19:45:03 +0100

    Day 22 PART I Solution

--- Day-22/01.js
+++ Day-22/01.js
@@ -6,7 +6,17 @@ const callback= (err, data) => {
 
     const [player1, player2] = parseInput(data);
 
-    console.log(player1, player2);
+    // console.log(player1, player2);
+
+    while(player1.length > 0 && player2.length > 0) {
+        nextRound(player1, player2);
+    }
+
+    const winner = player1.length > player2.length ? player1 : player2;
+
+    const output = getPlayerScore(winner);
+
+    console.log(output);
 }
 
 const parseInput = (input) => {
@@ -21,6 +31,21 @@ const parseInput = (input) => {
         );
 
     return [player1, player2];
+}
+
+const nextRound = (cardsP1, cardsP2) => {
+    const drawP1 = cardsP1.shift();
+    const drawP2 = cardsP2.shift();
+
+    const pool = [drawP1, drawP2].sort((a, b) => b - a);
+
+    return drawP1 > drawP2 ? cardsP1.push(...pool) : cardsP2.push(...pool);
+}
+
+const getPlayerScore = (cards) => {
+    return cards.reverse().reduce((acc, cur, idx) => {
+        return acc += cur * (idx + 1);
+    }, 0)
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
