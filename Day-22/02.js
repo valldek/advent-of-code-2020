53be58880509e5ea998ba25072e1bc6b0cad5437
valldek <valldek@gmail.com>
2021-01-01 20:36:04 +0100

    Day 22 PART II Solution

--- Day-22/02.js
+++ Day-22/02.js
@@ -6,11 +6,8 @@ const callback= (err, data) => {
 
     const [player1, player2] = parseInput(data);
 
-    // console.log(player1, player2);
-
-    while(player1.length > 0 && player2.length > 0) {
-        nextRound(player1, player2);
-    }
+    console.log(player1, player2);
+    playRecursiveCombat(player1, player2);
 
     const winner = player1.length > player2.length ? player1 : player2;
 
@@ -33,19 +30,43 @@ const parseInput = (input) => {
     return [player1, player2];
 }
 
-const nextRound = (cardsP1, cardsP2) => {
-    const drawP1 = cardsP1.shift();
-    const drawP2 = cardsP2.shift();
-
-    const pool = [drawP1, drawP2].sort((a, b) => b - a);
-
-    return drawP1 > drawP2 ? cardsP1.push(...pool) : cardsP2.push(...pool);
-}
-
 const getPlayerScore = (cards) => {
     return cards.reverse().reduce((acc, cur, idx) => {
         return acc += cur * (idx + 1);
     }, 0)
+}
+
+const playRecursiveCombat = (cardsP1, cardsP2, history = new Set()) => {
+    while(true) {
+        const drawP1 = cardsP1.shift();
+        const drawP2 = cardsP2.shift();
+
+        const state = cardsP1.join(',') + ',' + cardsP2.join(',');
+
+        if (history.has(state)) {
+            return 'p1';
+        } else {
+            history.add(state)
+        }
+
+        let winner = drawP1 > drawP2 ? 'p1' : 'p2';
+
+        if (cardsP1.length >= drawP1 && cardsP2.length >= drawP2) {
+            winner = playRecursiveCombat(cardsP1.slice(0, drawP1), cardsP2.slice(0, drawP2), new Set());
+        }
+
+        if (winner === 'p1') {
+            cardsP1.push(drawP1, drawP2);
+        } else {
+            cardsP2.push(drawP2, drawP1);
+        }
+
+        if (cardsP1.length === 0) {
+            return 'p2';
+        } else if (cardsP2.length === 0) {
+            return 'p1';
+        }
+    }
 }
 
 fs.readFile(path.join(__dirname, 'input'), 'utf-8', callback);
